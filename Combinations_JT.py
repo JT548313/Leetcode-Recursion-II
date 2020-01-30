e944756995ce6bb17a83c028395d6f634c89e34b
JT548313 <jayati.naik2@ibm.com>
2020-01-30 07:12:18 +0000

    combination

---
+++ Combinations_JT.py
@@ -0,0 +1,41 @@
+import copy
+from typing import List
+
+
+class Solution:
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        if n == 0:
+            return None
+
+        if k == 0:
+            return None
+
+        response = []
+        response_set = set()
+
+        def backtrack(counter=1, combination=[], x_number_list=[]):
+            if counter == k + 1:
+                return
+            for i in [y for y in range(1, n + 1) if y not in x_number_list]:
+                combination.append(i)
+                x_number_list.append(i)
+                if counter == k:
+                    temp = copy.deepcopy(sorted(combination))
+                    response.append(temp)
+                else:
+                    backtrack(counter + 1, combination, x_number_list)
+                combination.pop()
+                if counter != 1:
+                    x_number_list.pop()
+
+        backtrack()
+
+        unique_data = [list(x) for x in set(tuple(x) for x in response)]
+
+        return unique_data
+
+
+s = Solution()
+print(s.combine(4, 3))
